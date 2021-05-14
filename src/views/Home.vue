6c5bbcb03a93d0c8d58d6ffe28fad7eeb8747ac0
wojtek <wojtekgrelewicz@gmail.com>
2021-05-14 11:56:46 +0200

    dodane formatki

---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <hello-world />
+</template>
+
+<script>
+  import HelloWorld from '../components/Dashboard'
+
+  export default {
+    name: 'Home',
+
+    components: {
+      HelloWorld,
+    },
+  }
+</script>
---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    transpileDependencies: [
+        'vuetify',],
+
+    devServer: {
+        port: 8084,
+        host: '0.0.0.0'
+    }
+
+}
